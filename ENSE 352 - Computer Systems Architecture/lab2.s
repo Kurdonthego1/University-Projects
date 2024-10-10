39e64699a1379146b4f89eb7ce02f8a8748584d9
Zana Osman <thelegendaryzana@gmail.com>
2024-10-10 08:41:19 -0600

    New save

--- ENSE 352 - Computer Systems Architecture/lab2.s
+++ ENSE 352 - Computer Systems Architecture/lab2.s
@@ -32,6 +32,26 @@ __Vectors	DCD		INITIAL_MSP			; stack pointer value when stack is empty
 Reset_Handler	PROC
 
 ; Notice the stack at 0x200000FF8 (Contains R1 then LR ) Little endian
+	
+	BL phase1 ; Branch to the phase 1 function
+
+	BL phase2fact ; Branch to the factorial function
+
+String1
+		DCB "ENSE 352 is fun and I am learning ASM assembly!",0
+		
+String2
+		DCB "Yes I really love it!",0
+		
+	MOV R9, #0x0
+	LDR R6, String1
+	BL phase3 ; Branch to the phase 3 function
+	
+	MOV R9, #0x0
+	LDR R6, String2
+	BL phase3 ; Branch to the phase 3 function
+	
+phase1 PROC ; phase 1 function
 	MOV R0, #0x00001111 ;R0 will now be 0x00001111
 	MOV R1, #0x00002222 ;R1 will now be 0x00002222
 	MOV R2, #0x00003333 ;R2 will now be 0x00003333
@@ -45,20 +65,68 @@ Reset_Handler	PROC
 	PUSH{R0, R1, R2} ; Now this pushes all the values back on, the SP will now be R0 ^ R1 ^ R2 ^ R1 ^ R0 so it will have 5 on the stack and it will show on the memory
   
 	POP {R0, R1, R2} ; This will pop off the R0 ^ R1 ^ R2 from the SP
+	BX LR ; Exits the function and goes back to main
 	
-	
+ENDP
 
-	BL factorial ; Branch to the factorial function
+ALIGN
 
-
-factorial PROC ; factorial function	
-	MOV R4, 0x4 ; R4 is the register where we will calculate, using 4 as my integer number
+phase2fact PROC ; factorial function	
+	MOV R4, #4 ; R4 is the register where we will calculate, using 4 as my integer number
+	MOV R5, #1 ; 1 is always the case we end so we will move into R5
 	
+	PUSH {R5} ; Push R5 onto the stack pointer
 	
-	
-	
+p2f_loop
+	MUL R4, R5, R4 ; Multiply 
+	SUBS R3, #1 ; Subtracts 1 from R3 and allows the function to reduce
+	BNE p2f_loop ; Looks for zero flag or when R3 is zero, once it is it will exit the loop
 	
 	BX LR ; Exits the function and goes back to main
+ENDP
+	
+phase3 PROC
+	LDRB R9, [R6], #1
+	
+	CMP R9, #'A'
+	ADDEQ R8, R8, #1
+	
+	CMP R9, #'a'
+	ADDEQ R8, R8, #1
+	
+	CMP R9, #'E'
+	ADDEQ R8, R8, #1
+	
+	CMP R9, #'e'
+	ADDEQ R8, R8, #1
+
+	CMP R9, #'I'
+	ADDEQ R8, R8, #1
+	
+	CMP R9, #'i'
+	ADDEQ R8, R8, #1
+	
+	CMP R9, #'O'
+	ADDEQ R8, R8, #1
+	
+	CMP R9, #'o'
+	ADDEQ R8, R8, #1
+	
+	CMP R9, #'U'
+	ADDEQ R8, R8, #1
+	
+	CMP R9, #'u'
+	ADDEQ R8, R8, #1
+	
+	CMP R9, #'Y'
+	ADDEQ R8, R8, #1
+	
+	CMP R9, #'y'
+	ADDEQ R8, R8, #1
+	 
+	BL phase3 ; Once it hits 0 for R9 it will break out of the function otherwise it will keep going
+	
+	BX LR ; Exits the function and goes back to main 
 ENDP
 
 
